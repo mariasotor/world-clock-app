303da2b79d6bec09a5334c6c54a2c4293bbb7d54
mariasotor <alejandrasotor3@gmail.com>
2023-05-24 15:48:36 -0300

    Added functionality for the select

--- src/app.js
+++ src/app.js
@@ -6,17 +6,42 @@ function showTime(timezone) {
   return moment().tz(timezone).format("h:mm:ss [<small>]A[</small>]");
 }
 function updateTime() {
-  document.querySelector("#newyork-time").innerHTML =
-    showTime("America/New_York");
-  document.querySelector("#newyork-date").innerHTML =
-    showDate("America/New_York");
+  let NewYorkTimeElement = document.querySelector("#newyork-time");
+  if (NewYorkTimeElement) {
+    NewYorkTimeElement.innerHTML = showTime("America/New_York");
+    let NewYorkDateElement = document.querySelector("#newyork-date");
+    NewYorkDateElement.innerHTML = showDate("America/New_York");
 
-  document.querySelector("#tokyo-time").innerHTML = showTime("Asia/Tokyo");
-  document.querySelector("#tokyo-date").innerHTML = showDate("Asia/Tokyo");
+    let tokyoTimeElement = document.querySelector("#tokyo-time");
+    tokyoTimeElement.innerHTML = showTime("Asia/Tokyo");
+    let tokyoDateElement = document.querySelector("#tokyo-date");
+    tokyoDateElement.innerHTML = showDate("Asia/Tokyo");
 
-  document.querySelector("#bogota-time").innerHTML = showTime("America/Bogota");
-  document.querySelector("#bogota-date").innerHTML = showDate("America/Bogota");
+    let bogotaTimeElement = document.querySelector("#bogota-time");
+    bogotaTimeElement.innerHTML = showTime("America/Bogota");
+    let bogotaDateElement = document.querySelector("#bogota-date");
+    bogotaDateElement.innerHTML = showDate("America/Bogota");
+  }
 }
+
+function updateCity(event) {
+  let cityDisplayElement = document.querySelector("#city-display");
+  cityDisplayElement.innerHTML = `
+  <div class="row align-items-center">
+  <div class="col-6">
+  <h2 class="city">
+  ${event.target.selectedOptions[0].text}
+  </h2>
+  <div class="date">${showDate(event.target.value)}</div>
+  </div>
+  <div class="col-6">
+  <div class="time">${showTime(event.target.value)}</div>
+  </div>
+  </div>`;
+}
+
+let selectCityElement = document.querySelector("#cities");
+selectCityElement.addEventListener("change", updateCity);
 
 updateTime();
 setInterval(updateTime, 1000);
